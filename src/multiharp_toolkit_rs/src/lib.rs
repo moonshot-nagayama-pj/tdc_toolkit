0de337be3ea010b8a933111a06528d9da44acc41
zigen <hrlclb@gmail.com>
2023-12-17 08:27:20 +0000

    remove parser-rs

--- src/multiharp_toolkit_rs/src/lib.rs
+++ src/multiharp_toolkit_rs/src/lib.rs
@@ -1,14 +1,21 @@
-use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+mod mhlib_wrapper;
+// use pyo3::prelude::*;
+// #[allow(non_upper_case_globals)]
+// #[allow(non_camel_case_types)]
+// #[allow(non_snake_case)]
 
-/// A Python module implemented in Rust.
-#[pymodule]
-fn multiharp_toolkit(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
-}
+// /// Formats the sum of two numbers as string.
+// #[pyfunction]
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
+
+// /// A Python module implemented in Rust.
+// #[pymodule]
+// #[pyo3(name = "multiharp_toolkit_rs")]
+// fn multiharp_toolkit(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+//     // m.add_function(wrap_pyfunction!(MH_GetLibraryVersion, m)?)?;
+//     Ok(())
+// }
